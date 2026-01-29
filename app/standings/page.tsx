4de972bdd76d3eec59ee776b553bb5e6b45bbcba
B-Gilb022 <benjamin.gilbert2004@icloud.com>
2026-01-28 22:09:09 -0500

    Added Initial Landing and Standings Pages

---
+++ app/standings/page.tsx
@@ -0,0 +1,9 @@
+export default function Standings() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-800 text-white">
+      <h2 className="text-3xl font-semibold">
+        Standings Coming Soon 🏆
+      </h2>
+    </main>
+  );
+}
